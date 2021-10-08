4a47720b129f12d57660cc99652e372505f2fbc3
Matias Vazquez <matias.vazquez@tec.mx>
2021-10-08 14:10:44 -0500

    added datatypes example

---
+++ partial02/datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+	printf("Size of int is %d byte(s)\n", sizeof(int));
+	printf("Size of unsigned int is %d byte(s)\n", sizeof(unsigned int));
+	printf("Size of short is %d byte(s)\n", sizeof(short));
+	printf("Size of unsigned short is %d byte(s)\n", sizeof(unsigned short));
+	printf("Size of long is %d byte(s)\n", sizeof(long));
+	printf("Size of char is %d byte(s)\n", sizeof(char));
+	printf("Size of float is %d byte(s)\n", sizeof(float));
+	printf("Size of double is %d byte(s)\n", sizeof(double));
+}
